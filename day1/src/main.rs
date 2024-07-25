603466b719e4e296443f8b642c895d97a470c3dd
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2024-07-25 08:03:02 +0200

    almost finished day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::u32;
+
 fn main() {
-    println!("Hello, world!");
+    let file =
+        BufReader::new(File::open("content/exampleInput.txt").expect("Cannot open input.txt"));
+
+    input_vec(file);
+}
+
+fn input_vec(file: BufReader<File>) {
+    for line in file.lines() {
+        if let Ok(line) = line {
+            let number_vec: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
+            eprintln!("{:?}", number_vec);
+        }
+    }
 }
